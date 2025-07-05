b4414492ec8e79a10ebd3c5c991f27b096a9538d
Urval <kheniurval777@gmail.com>
2025-07-05 18:01:29 +0530

    Final Commit

---
+++ Codes/Basic/Datatype.java
@@ -0,0 +1,25 @@
+package Codes.Basic;
+
+public class Datatype {
+    public static void main(String []args){
+        // Integer data type
+        int num = 42;
+        System.out.println("Integer: " + num);
+
+        // Floating-point data type
+        float decimalNum = 3.14f;
+        System.out.println("Float: " + decimalNum);
+
+        // Double data type
+        double largeDecimalNum = 3.141592653589793;
+        System.out.println("Double: " + largeDecimalNum);
+
+        // Character data type
+        char letter = 'A';
+        System.out.println("Character: " + letter);
+
+        // Boolean data type
+        boolean isTrue = true;
+        System.out.println("Boolean: " + isTrue);
+    }
+}
