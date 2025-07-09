457240df6f8f9ba065f18f17ec5f095855a31e81
Urval <kheniurval777@gmail.com>
2025-07-09 10:59:48 +0530

    Class task4 is done

---
+++ Codes/BasicClg/Lecture/Task4.java
@@ -0,0 +1,35 @@
+package Codes.BasicClg.Lecture;
+abstract class Game{
+    abstract void play();    
+    public void start() {
+        System.out.println("Game is starting...");
+    }
+    
+    public void end() {
+        System.out.println("Game has ended.");
+    }
+}
+class Cricket extends Game{
+    void play() {
+        System.out.println("Playing Cricket");
+    }
+}
+class Tennis extends Game{
+    void play() {
+        System.out.println("Playing Tennis");
+    }
+}
+public class Task4 {
+    public static void main(String[] args) {
+        Game cricket = new Cricket();
+        Game tennis = new Tennis();
+        
+        cricket.start();
+        cricket.play();
+        cricket.end();
+        System.out.println();
+        tennis.start();
+        tennis.play();
+        tennis.end();
+    }
+}
