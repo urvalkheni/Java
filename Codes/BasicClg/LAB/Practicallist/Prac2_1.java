e6d4ecac98e77b21141b1597fae999f423ca7930
Urval <kheniurval777@gmail.com>
2025-07-28 15:26:16 +0530

    Final Commit

--- Codes/BasicClg/LAB/Practicallist/Prac2_1.java
+++ Codes/BasicClg/LAB/Practicallist/Prac2_1.java
@@ -1,7 +1,47 @@
-package Codes.BasicClg.LAB.Practicallist;
+// package Codes.BasicClg.LAB.Practicallist;
+
+import java.util.*;
+
+class Sanitizer {
+    private static final Set<String> INAPPROPRIATE_WORDS = new HashSet<>(Arrays.asList(
+        "damn", "hell", "shit", "crap", "wtf", "lol",
+    ));
+
+    public static String removeExtraSpaces(String input) {
+        return input.trim().replaceAll("\\s+", " ");
+    }
+
+    public static String replaceInappropriateWords(String input) {
+        String[] words = input.split(" ");
+        for (int i = 0; i < words.length; i++) {
+            String wordClean = words[i].replaceAll("[^a-zA-Z]", "").toLowerCase();
+            if (INAPPROPRIATE_WORDS.contains(wordClean)) {
+                words[i] = words[i].replaceAll("(?i)" + wordClean, "***");
+            }
+        }
+        return String.join(" ", words);
+    }
+
+    public static String formatCapitalization(String input) {
+        if (input.isEmpty()) return input;
+        input = input.toLowerCase();
+        return Character.toUpperCase(input.charAt(0)) + input.substring(1);
+    }
+
+    public static String sanitize(String input) {
+        String step1 = removeExtraSpaces(input);
+        String step2 = replaceInappropriateWords(step1);
+        String step3 = formatCapitalization(step2);
+        return step3;
+    }
+}
 
 public class Prac2_1 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your chat message:");
+        String input = scanner.nextLine();
+        String sanitized = Sanitizer.sanitize(input);
+        System.out.println("Sanitized message: " + sanitized);
     }
 }
